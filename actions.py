a62a0b1b4c6c849924bb1954a3cc6f651c600347
внучко валерия <werbix706@gmail.com>
2026-09-24 14:27:02 +0300

    homework3.1

---
+++ actions.py
@@ -0,0 +1,16 @@
+a = 200
+
+if a < 300:
+    print ("a is getattr 300")
+    a= a + 300
+elif a > 500:
+    print ("a is getattr 500")
+    a= a - 500
+elif a * 0.5:
+    print ("a is getattr 0.5")
+    a= a * 0.5
+elif a / 2:
+    print ("a is getattr 2")
+    a= a / 2
+else:
+    print("a is getattr 3")
